293ec6cbbbd3907948d23cd6c6fcc9783ece6e2a
Son Pham <son.pham@worc.ox.ac.uk>
2011-07-07 17:57:36 +0100

    - Added code to try out the button in Main.java

--- src/org/mollyproject/android/Main.java
+++ src/org/mollyproject/android/Main.java
@@ -2,9 +2,12 @@ package org.mollyproject.android;
 
 import org.json.JSONObject;
 import org.mollyproject.android.controller.Router;
+import org.mollyproject.android.selection.SelectionManager;
 import org.mollyproject.android.view.Renderer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends Activity {
@@ -28,9 +31,18 @@ public class Main extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
-				
-        TextView tv = new TextView(this);
-        tv.setText(o.toString());
-        setContentView(tv);
+		//Code to try out the button, failed miserably
+		final Button button = (Button) findViewById(R.id.button);
+		
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	try {
+					router.onRequestSent(router.getRenderer().getSelectionManager().getStringLocator(SelectionManager.ViewNames.places_index));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
     }
 }
