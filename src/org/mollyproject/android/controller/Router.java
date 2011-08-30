ace5d480d6eccb724b5ca56fd995e7c3c1c79730
famanson <son.pham@worc.ox.ac.uk>
2011-08-30 15:27:32 +0100

    Support for GZip

--- src/org/mollyproject/android/controller/Router.java
+++ src/org/mollyproject/android/controller/Router.java
@@ -11,13 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
@@ -25,6 +29,16 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.location.Location;
+
+
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpException;
+import org.apache.http.entity.HttpEntityWrapper;
+import org.apache.http.protocol.HttpContext;
 
 public class Router {
 	protected CookieManager cookieMgr;
@@ -47,7 +61,64 @@ public class Router {
 		locTracker.startLocUpdate();
 		client = new DefaultHttpClient();
 		client.getParams().setParameter("http.connection-manager.timeout",10000);
+		
+		((DefaultHttpClient) client).addRequestInterceptor(new HttpRequestInterceptor() {
+
+            public void process(
+                    final HttpRequest request,
+                    final HttpContext context) throws HttpException, IOException {
+                if (!request.containsHeader("Accept-Encoding")) {
+                    request.addHeader("Accept-Encoding", "gzip");
+                }
+                //TODO: add the location updates header here when it is implemented on the Molly server
+                //e.g: X-Current-Location: -1.6, 51, 100
+            }
+        });
+
+        ((DefaultHttpClient) client).addResponseInterceptor(new HttpResponseInterceptor() {
+            public void process(
+                    final HttpResponse response,
+                    final HttpContext context) throws HttpException, IOException {
+                HttpEntity entity = response.getEntity();
+                Header ceheader = entity.getContentEncoding();
+                if (ceheader != null) {
+                    HeaderElement[] codecs = ceheader.getElements();
+                    for (int i = 0; i < codecs.length; i++) {
+                        if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                            response.setEntity(
+                                    new GzipDecompressingEntity(response.getEntity()));
+                            return;
+                        }
+                    }
+                }
+            }
+
+        });
 	}
+	
+	static class GzipDecompressingEntity extends HttpEntityWrapper {
+
+        public GzipDecompressingEntity(final HttpEntity entity) {
+            super(entity);
+        }
+
+        @Override
+        public InputStream getContent()
+            throws IOException, IllegalStateException {
+
+            // the wrapped entity's getContent() decides about repeatability
+            InputStream wrappedin = wrappedEntity.getContent();
+
+            return new GZIPInputStream(wrappedin);
+        }
+
+        @Override
+        public long getContentLength() {
+            // length of ungzipped content is not known
+            return -1;
+        }
+
+    }
     
 	public void setApp(MyApplication myApp)
     {
@@ -89,7 +160,8 @@ public class Router {
 	}
 	
 	public synchronized JSONObject onRequestSent(String locator, String arg, OutputFormat format, String query) 
-							throws JSONException, UnknownHostException, IOException, ParseException {
+				throws JSONException, UnknownHostException, IOException, ParseException, IllegalArgumentException 
+	{
 		/*basic method for all requests for json response, it sets up a url to be sent
 		  to the server as follow:
 		  1. it looks up the url to the required page using the reverse api with either the
@@ -105,8 +177,8 @@ public class Router {
 		*/
 		if (!firstReq)
 		{
-			client = new DefaultHttpClient();
-			client.getParams().setParameter("http.connection-manager.timeout", 20000);
+			//client = new DefaultHttpClient();
+			//client.getParams().setParameter("http.connection-manager.timeout", 20000);
 			((DefaultHttpClient)client).setCookieStore(cookieMgr.getCookieStore());
 			System.out.println("Cookie set");
 			updateCurrentLocation(locTracker.getCurrentLoc());
