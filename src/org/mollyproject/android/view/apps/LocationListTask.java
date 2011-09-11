85369433a1c428cf918f486e1dc82db4812ac4d9
famanson <son.pham@worc.ox.ac.uk>
2011-09-11 21:24:21 +0700

    Code for updating the suggestions layouts (every time the location dialog is opened and when the new location is updated)

--- src/org/mollyproject/android/view/apps/LocationListTask.java
+++ src/org/mollyproject/android/view/apps/LocationListTask.java
@@ -9,6 +9,7 @@ import org.mollyproject.android.controller.MyApplication;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,56 +29,133 @@ public class LocationListTask extends BackgroundTask<View, Void, View[]>{
 
 	@Override
 	public void updateView(View[] views) {
-		//views[0] should be the historyLayout and view[1] should be the currentLocation TextView
+		//views[0] should be the historyLayout and view[1] should be the suggestionsLayout and view[2] is currentLocation TextView
 		final LinearLayout historyLayout = (LinearLayout) views[0];
-		final TextView currentLocation = (TextView) views[1];
+		final LinearLayout suggestionsLayout = (LinearLayout) views[1];
+		final RelativeLayout showHistoryLayout = (RelativeLayout) views[2];
+		final RelativeLayout showSuggestionsLayout = (RelativeLayout) views[3];
+		final TextView currentLocation = (TextView) views[4];
 		try {
-			updateLocationLayout(historyLayout, currentLocation);
+			//for use in the location dialog only
+			if (MyApplication.currentLocation != null)
+			{
+				//update the currentLocation text
+				Page.updateLocText(currentLocation);
+				
+				/* Each JSONObject in the alternatives (suggestions) and history JSONArrays should have:
+				 * String name
+				 * JSONArray location, with 0: Double lat and 1: Double lon
+				 * Double accuracy
+				 */
+				
+				//populate the history
+				JSONArray locationHistory = MyApplication.currentLocation.getJSONArray("history");
+				if (locationHistory.length() == 0)
+				{
+					//Disable the history function
+					showHistoryLayout.setBackgroundResource(R.drawable.shape_gray);
+					showHistoryLayout.setClickable(false);
+				}
+				else
+				{
+					//Enable it, update the historyLayout
+					showHistoryLayout.setBackgroundResource(R.drawable.bg_blue);
+					showHistoryLayout.setClickable(true);
+					showHistoryLayout.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							page.showDialog(Page.DIALOG_LOCATION_HISTORY);
+						}
+					});
+					
+					historyLayout.removeAllViews();
+					for (int i = 0; i < locationHistory.length(); i++)
+					{
+						final JSONObject location = locationHistory.getJSONObject(i);
+						LinearLayout historyResult = (LinearLayout) page.getLayoutInflater().inflate
+									(R.layout.plain_text_search_result, null);
+						historyResult.setLayoutParams(Page.paramsWithLine);
+						//get the name of the location and display it
+						TextView historyResultName = (TextView) historyResult.findViewById(R.id.plainTextResultText);
+						historyResultName.setText(location.getString("name"));
+						//make the result clickable
+						historyResult.setClickable(true);
+						historyResult.setBackgroundResource(R.drawable.bg_blue);
+						historyResult.setOnClickListener(new OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								try {
+									//get new location and update the current location text and the history list
+									new LocationListTask(location.getString("name"), location.getJSONArray("location").getDouble(0), 
+											location.getJSONArray("location").getDouble(1), location.getDouble("accuracy"), page, false, true)
+												.execute(historyLayout, suggestionsLayout, showHistoryLayout, showSuggestionsLayout, currentLocation);
+								} catch (Exception e) {
+									Toast.makeText(page, "Your new location cannot be updated. Please try " +
+											"again later", Toast.LENGTH_SHORT).show();
+								}
+								page.dismissDialog(Page.DIALOG_LOCATION_HISTORY);
+							}
+						});
+						historyLayout.addView(historyResult);
+					}
+				}
+				
+				JSONArray alternatives = MyApplication.currentLocation.getJSONArray("alternatives");
+				if (alternatives.length() == 0)
+				{
+					//Disable the suggestions function
+					showSuggestionsLayout.setBackgroundResource(R.drawable.shape_gray);
+					showSuggestionsLayout.setClickable(false);
+				}
+				else
+				{
+					//Enable it, update the suggestionsLayout
+					showSuggestionsLayout.setBackgroundResource(R.drawable.bg_blue);
+					showSuggestionsLayout.setClickable(true);
+					showSuggestionsLayout.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							page.showDialog(Page.DIALOG_LOCATION_SUGGESTIONS);
+						}
+					});
+					suggestionsLayout.removeAllViews();
+					for (int i = 0; i < alternatives.length(); i++)
+					{
+						final JSONObject alternative = alternatives.getJSONObject(i);
+						LinearLayout alternativeResult = (LinearLayout) page.getLayoutInflater().inflate
+								(R.layout.plain_text_search_result, null);
+						alternativeResult.setLayoutParams(Page.paramsWithLine);
+						//get the name of the location and display it
+						TextView alternativeResultName = (TextView) alternativeResult.findViewById(R.id.plainTextResultText);
+						alternativeResultName.setText(alternative.getString("name"));
+						//make the result clickable
+						alternativeResult.setClickable(true);
+						alternativeResult.setBackgroundResource(R.drawable.bg_blue);
+						alternativeResult.setOnClickListener(new OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								try {
+									//get new location and update the current location text and the alternatives list
+									new LocationListTask(alternative.getString("name"), alternative.getJSONArray("location").getDouble(0), 
+											alternative.getJSONArray("location").getDouble(1), alternative.getDouble("accuracy"), page, false, true)
+												.execute(historyLayout, suggestionsLayout, showHistoryLayout, showSuggestionsLayout, currentLocation);
+								} catch (Exception e) {
+									Toast.makeText(page, "Your new location cannot be updated. Please try " +
+											"again later", Toast.LENGTH_SHORT).show();
+								}
+								page.dismissDialog(Page.DIALOG_LOCATION_SUGGESTIONS);
+							}
+						});
+						suggestionsLayout.addView(alternativeResult);
+					}
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			//otherException = true;
 		}
 	}
 	
-	private void updateLocationLayout(final LinearLayout historyLayout, final TextView currentLocation) throws Exception
-	{
-		//for use in the location dialog only
-		if (MyApplication.currentLocation != null)
-		{
-			Page.updateLocText(currentLocation);
-			JSONArray locationHistory = MyApplication.currentLocation.getJSONArray("history");
-			historyLayout.removeAllViews();
-			for (int i = 0; i < locationHistory.length(); i++)
-			{
-				final JSONObject location = locationHistory.getJSONObject(i);
-				LinearLayout historyResult = (LinearLayout) page.getLayoutInflater().inflate
-							(R.layout.plain_text_search_result, null);
-				historyResult.setLayoutParams(Page.paramsWithLine);
-				//get the name of the location and display it
-				TextView historyResultName = (TextView) historyResult.findViewById(R.id.plainTextResultText);
-				historyResultName.setText(location.getString("name"));
-				//make the result clickable
-				historyResult.setClickable(true);
-				historyResult.setBackgroundResource(R.drawable.bg_blue);
-				historyResult.setOnClickListener(new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						try {
-							//get new location and update the current location text and the history list
-							new LocationListTask(location.getString("name"), location.getJSONArray("location").getDouble(0), 
-									location.getJSONArray("location").getDouble(0), location.getDouble("accuracy"), page, false, true)
-										.execute(historyLayout,currentLocation);
-						} catch (Exception e) {
-							Toast.makeText(page, "Your new location cannot be updated. Please try " +
-									"again later", Toast.LENGTH_SHORT).show();
-						}
-					}
-				});
-				historyLayout.addView(historyResult);
-			}
-		}
-	}
-
 	@Override
 	protected View[] doInBackground(View... params) {
 		// TODO Auto-generated method stub
