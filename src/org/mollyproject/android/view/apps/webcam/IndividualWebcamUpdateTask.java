d1e96fb9d42197f54f41c1ed04c41f4ba7dd2a39
famanson <son.pham@worc.ox.ac.uk>
2011-09-19 00:30:11 +0700

    Added a seperate task for downloading the webcam image

--- src/org/mollyproject/android/view/apps/webcam/IndividualWebcamUpdateTask.java
+++ src/org/mollyproject/android/view/apps/webcam/IndividualWebcamUpdateTask.java
@@ -1,5 +1,6 @@
 package org.mollyproject.android.view.apps.webcam;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -7,10 +8,13 @@ import java.util.Date;
 
 import org.json.JSONObject;
 import org.mollyproject.android.R;
+import org.mollyproject.android.controller.BackgroundTask;
 import org.mollyproject.android.controller.JSONProcessingTask;
 import org.mollyproject.android.controller.MyApplication;
 import org.mollyproject.android.controller.Router;
 import org.mollyproject.android.view.apps.ContentPage;
+import org.mollyproject.android.view.apps.Page;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -20,18 +24,23 @@ import android.widget.TextView;
 
 public class IndividualWebcamUpdateTask extends JSONProcessingTask {
 	protected Bitmap bitmap;
+	protected LinearLayout webcamLayout;
 	public IndividualWebcamUpdateTask(ContentPage page,
 			boolean toDestroyPageAfterFailure, boolean dialogEnabled) {
 		super(page, toDestroyPageAfterFailure, dialogEnabled);
-		// TODO Auto-generated constructor stub
+		webcamLayout = page.getContentLayout();
 	}
 
 	@Override
 	public void updateView(JSONObject jsonContent) {
 		try {
+			URL url = new URL(jsonContent.getJSONObject("webcam").getString("url"));
+			
+			new SingleImageDownload(page, false, true).execute(url);
+			
 			//remember: the webcamLayout is now contentLayout
 			JSONObject webcam = jsonContent.getJSONObject("webcam");
-			LinearLayout webcamLayout = page.getContentLayout();//(LinearLayout) page.getLayoutInflater().inflate(R.layout.webcam, null);
+			//(LinearLayout) page.getLayoutInflater().inflate(R.layout.webcam, null);
 			
 			TextView webcamName = (TextView) webcamLayout.findViewById(R.id.webcamName);
 			webcamName.setText(webcam.getString("title") + '\n' + MyApplication.hourFormat.format(new Date()));
@@ -48,8 +57,6 @@ public class IndividualWebcamUpdateTask extends JSONProcessingTask {
 			}
 			webcamDetails.setText(description);
 			
-			ImageView webcamImage = (ImageView) webcamLayout.findViewById(R.id.webcamImage);
-			webcamImage.setImageBitmap(bitmap);
 		} catch (Exception e) {
 			e.printStackTrace();
 			otherException = true;
@@ -76,32 +83,65 @@ public class IndividualWebcamUpdateTask extends JSONProcessingTask {
 			}
 			//download the image, these images cannot be reused so no need to cache them
 			
-			URL url = new URL(jsonContent.getJSONObject("webcam").getString("url"));
-			HttpURLConnection conn= (HttpURLConnection)url.openConnection();
-			conn.setDoInput(true);
-			conn.connect();
-			InputStream is = conn.getInputStream();
-			bitmap = BitmapFactory.decodeStream(is);
-			
-			//matrix used to resize image:
-	        int width = bitmap.getWidth();
-	        int height = bitmap.getHeight();
-	        int newWidth = page.getWindow().getWindowManager().getDefaultDisplay().getWidth();
-	        int newHeight = page.getWindow().getWindowManager().getDefaultDisplay().getWidth()*3/4;
-	       
-	        float scaleWidth = ((float) newWidth) / width;
-	        float scaleHeight = ((float) newHeight) / height;
-	       
-	        Matrix matrix = new Matrix();
-	        //resize the bitmap
-	        matrix.postScale(scaleWidth, scaleHeight);
-	        bitmap = Bitmap.createBitmap(bitmap, 0, 0,
-                      width, height, matrix, true);
 		} catch (Exception e) {
 			e.printStackTrace();
 			otherException = true;
 		}
 		return jsonContent;
 	}
+	
+	private class SingleImageDownload extends BackgroundTask<URL, Void, String>
+	{
 
+		public SingleImageDownload(Page page,
+				boolean toDestroyPageAfterFailure, boolean dialogEnabled) {
+			super(page, toDestroyPageAfterFailure, dialogEnabled);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void updateView(String outputs) {
+			try {
+				ImageView webcamImage = (ImageView) webcamLayout.findViewById(R.id.webcamImage);
+				webcamImage.setImageBitmap(bitmap);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		protected String doInBackground(URL... params) {
+			try {
+				HttpURLConnection conn= (HttpURLConnection)params[0].openConnection();
+				conn.setDoInput(true);
+				conn.connect();
+				InputStream is;
+				
+				is = conn.getInputStream();
+				
+				bitmap = BitmapFactory.decodeStream(is);
+				
+				//matrix used to resize image:
+		        int width = bitmap.getWidth();
+		        int height = bitmap.getHeight();
+		        int newWidth = page.getWindow().getWindowManager().getDefaultDisplay().getWidth();
+		        int newHeight = page.getWindow().getWindowManager().getDefaultDisplay().getWidth()*3/4;
+		       
+		        float scaleWidth = ((float) newWidth) / width;
+		        float scaleHeight = ((float) newHeight) / height;
+		       
+		        Matrix matrix = new Matrix();
+		        //resize the bitmap
+		        matrix.postScale(scaleWidth, scaleHeight);
+		        bitmap = Bitmap.createBitmap(bitmap, 0, 0,
+	                      width, height, matrix, true);
+				return "done downloading";
+			} catch (IOException e) {
+				e.printStackTrace();
+				otherException = true;
+			}
+			return null;
+		}
+		
+	}
 }
