e5aa3b39eada5018a34e92a025bbd6736c3adeb5
famanson <son.pham@worc.ox.ac.uk>
2011-09-22 02:23:56 +0700

    Spinner for UnimplementedPage

--- src/org/mollyproject/android/view/apps/unimplemented/MollyWebClient.java
+++ src/org/mollyproject/android/view/apps/unimplemented/MollyWebClient.java
@@ -8,12 +8,27 @@ import java.util.Map;
 import org.mollyproject.android.controller.MollyModule;
 import org.mollyproject.android.controller.MyApplication;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MollyWebClient extends WebViewClient {
+	protected ProgressDialog pDialog;
+		
+	@Override
+	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+		super.onPageStarted(view, url, favicon);
+		pDialog = ProgressDialog.show(view.getContext(), null, "Loading");
+	}
+	
+	@Override
+	public void onPageFinished(WebView view, String url) {
+		super.onPageFinished(view, url);
+		pDialog.dismiss();
+	}	
 	@Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
 		System.out.println(url);
