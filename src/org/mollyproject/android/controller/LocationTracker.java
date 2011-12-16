0a9fa3820e8d24ca4dc688aa115c342b01eea8d2
famanson <son@esplorio.com>
2011-12-16 09:58:38 +0000

    javadoc comments for LocationTracker

--- src/org/mollyproject/android/controller/LocationTracker.java
+++ src/org/mollyproject/android/controller/LocationTracker.java
@@ -7,15 +7,45 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+/**
+ * class responsible for keeping track and updating of the current location of the device
+ * 
+ * @author famanson
+ *
+ */
 public class LocationTracker {
+	/**
+	 * a reference to the application state
+	 */
 	protected MyApplication myApp;
+	/**
+	 * a LocationManager object, used to communicate with the location service
+	 */
 	protected LocationManager locMgr;
+	/**
+	 * holds the location provider (e.g. GPS, network...)
+	 */
 	protected String provider;
+	/**
+	 * the default longitude, guess where this is
+	 */
 	public static double DEFAULT_LON = -1.255939;
+	/**
+	 * the default latitude, guess where this is
+	 */
 	public static double DEFAULT_LAT = 51.752527;
+	/**
+	 * the (last updated) location
+	 */
 	protected Location loc;
+	/**
+	 * specifies whether the location should be automatically updated or it is updated manually (in which case automatic update should be disabled)
+	 */
 	public static boolean autoLoc;
-	
+	/**
+	 * the class constructor sets the criteria when the location service should be run and initiates the required parameters
+	 * @param myApp the application state
+	 */
 	public LocationTracker(MyApplication myApp)
 	{
 		this.myApp = myApp;
@@ -37,12 +67,20 @@ public class LocationTracker {
 		autoLoc = true;
 	}
 	
+	/**
+	 * set the current location to a given pair of (lat, lon)
+	 * @param lat the given latitude
+	 * @param lon the given longitude
+	 */
 	public void setLocation(double lat, double lon)
 	{
 		loc.setLatitude(lat);
 		loc.setLongitude(lon);
 	}
 	
+	/**
+	 * a location listener
+	 */
 	private final LocationListener locationListener = new LocationListener() {
 	    public void onLocationChanged(Location location) {
 	       
@@ -59,21 +97,35 @@ public class LocationTracker {
 	    }
     };
     
+    /**
+     * checks if automatic location update is in use
+     * @return if autoLoc is true
+     */
     public boolean isAutoLoc()
     {
     	return autoLoc;
     }
-	    
+	/**
+	 * enable/disable the automatic location update
+	 * @param b a boolean value (true/false)
+	 */
 	public void setAutoLoc(boolean b)
 	{
 		autoLoc = b;
 	}
 	
+	/**
+	 * request a location update from the LocationManager
+	 */
 	public void startLocUpdate()
 	{
 		locMgr.requestLocationUpdates(provider, 0, 0,locationListener);
 	}
 	
+	/**
+	 * query the current (actually last updated) location
+	 * @return the last known location
+	 */
 	public Location getCurrentLoc()
 	{
 		if (autoLoc)
